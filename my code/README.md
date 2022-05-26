fe1824051431ac5b85d0663a9220c7313a42f21e
mrzh1 <87999315+mrzh1@users.noreply.github.com>
2022-05-26 17:54:37 +0430

    Add files via upload

---
+++ my code/README.md
@@ -0,0 +1,9 @@
+###******************************************************************************
+# Copyright (C) 2021 by Marzieh Abbasi - University of ferdowsi 
+#
+# 
+###*****************************************************************************
+
+
+### This is a free access repository for the  Embeded system in AI 
+
---
+++ my code/main.c
@@ -0,0 +1,15 @@
+/******************************************************************************
+ * Copyright (C) 2021 by MArzieh Abbasi - University of ferdowsi
+ *
+ 
+ *****************************************************************************/
+
+#include <stdio.h>
+/* A pretty boring main file */
+int main(void){
+
+  printf("\nModule Demo File!\n");
+
+  return 0;
+}
+
