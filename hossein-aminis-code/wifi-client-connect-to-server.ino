2340d52b9be12df2c4e6028fe85fabed873ee4ad
Hossein <hosseinaminiii75@gmail.com>
2022-05-27 16:55:41 +0430

    add a file for Exercise#3!

---
+++ hossein-aminis-code/wifi-client-connect-to-server.ino
@@ -0,0 +1,99 @@
+// Author: Hossein Amini
+// this code works on ESP-LoLin32 board.
+
+
+#include <WiFi.h>
+
+const char* ssid     = <WIFI-SSID>;
+const char* password = <WIFI-password>;
+
+const char* host = <Server-Address>;
+const char* streamId   = "api";
+const char* privateKey = "consumption";
+
+int value = 0;
+
+void INCREMENT(){
+  value ++;
+  Serial.print("value = ");
+  Serial.println(value);
+}
+
+
+
+void setup()
+{
+    Serial.begin(115200);
+    delay(10);
+
+    // We start by connecting to a WiFi network
+
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    pinMode(32, INPUT);
+    attachInterrupt(32, INCREMENT, RISING);
+}
+
+void loop()
+{
+    delay(10000);
+////    ++value;
+//
+    Serial.print("connecting to ");
+    Serial.println(host);
+
+    // Use WiFiClient class to create TCP connections
+    WiFiClient client;
+    const int httpPort = 80;
+    if (!client.connect(host, httpPort)) {
+        Serial.println("connection failed");
+        return;
+    }
+
+    // We now create a URI for the request
+    String url = "/api/consumption";
+    url += "?node=";
+    url += "node1";
+    url += "&consumption=";
+    url += value;
+
+    Serial.print("Requesting URL: ");
+    Serial.println(url);
+
+    // This will send the request to the server
+    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+                 "Host: " + host + "\r\n" +
+                 "Connection: close\r\n\r\n");
+    unsigned long timeout = millis();
+    while (client.available() == 0) {
+        if (millis() - timeout > 5000) {
+            Serial.println(">>> Client Timeout !");
+            client.stop();
+            return;
+        }
+    }
+
+    // Read all the lines of the reply from server and print them to Serial
+    while(client.available()) {
+        String line = client.readStringUntil('\r');
+        Serial.print(line);
+    }
+
+    Serial.println();
+    Serial.println("closing connection");
+}
+
